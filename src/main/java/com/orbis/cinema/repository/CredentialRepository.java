1062cb48305e63a2b26b64b5adb9f91e517c1e1d
zaccalos7 <guarin719@outlook.it>
2025-11-16 21:40:28 +0100

    repository for login

---
+++ src/main/java/com/orbis/cinema/repository/CredentialRepository.java
@@ -0,0 +1,9 @@
+package com.orbis.cinema.repository;
+
+import com.orbis.cinema.model.Credential;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CredentialRepository extends JpaRepository<Credential, Integer> {
+}
---
+++ src/main/java/com/orbis/cinema/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.orbis.cinema.repository;
+
+import com.orbis.cinema.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
