d28e9c35cb8a463ccf823b573f56b69ea522030b
zaccalos7 <guarin719@outlook.it>
2025-11-19 20:45:54 +0100

    dummy service

---
+++ src/main/java/com/orbis/cinema/service/LoginService.java
@@ -0,0 +1,12 @@
+package com.orbis.cinema.service;
+
+import com.orbis.cinema.inputRequest.RegisterRecord;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+    public boolean register(RegisterRecord registerRecord){
+        return false;
+    }
+}
