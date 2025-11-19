1b68d182d4a6b6dfe9ee6e04df557f98d99eb763
zaccalos7 <guarin719@outlook.it>
2025-11-19 22:14:47 +0100

    handling error

--- src/main/java/com/orbis/cinema/restController/LoginController.java
+++ src/main/java/com/orbis/cinema/restController/LoginController.java
@@ -1,8 +1,12 @@
 package com.orbis.cinema.restController;
 
+import com.orbis.cinema.component.LoggerMessageComponent;
 import com.orbis.cinema.inputRequest.RegisterRecord;
 import com.orbis.cinema.responseHandler.ResponseHandler;
 import com.orbis.cinema.service.LoginService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,26 +16,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/")
 public class LoginController {
 
     private final LoginService loginService;
     private final ResponseHandler responseHandler;
-
-    public LoginController(LoginService loginService, ResponseHandler responseHandler) {
-        this.loginService = loginService;
-        this.responseHandler = responseHandler;
-    }
+    private final LoggerMessageComponent loggerMessageComponent;
 
     @PostMapping("register")
     public ResponseEntity<Map<String, String>> register(@RequestBody RegisterRecord registerRecord){
+        ResponseEntity<Map<String, String>> mapResponseEntity;
+        String message;
         boolean response = loginService.register(registerRecord);
-        ResponseEntity<Map<String, String>> mapResponseEntity = response
-                ? responseHandler.buildResponse("success.register.response", HttpStatus.CREATED)
-                : responseHandler.buildResponse("error.register.response", HttpStatus.NOT_ACCEPTABLE);
 
+        if(response){
+            mapResponseEntity= responseHandler.buildResponse("success.register.response", HttpStatus.CREATED);
+            message = loggerMessageComponent.printMessage("success.register.response");
+            log.info("{} {}", message, HttpStatus.CREATED);
+            return mapResponseEntity;
+        }
+
+        mapResponseEntity= responseHandler.buildResponse("error.register.response", HttpStatus.NOT_ACCEPTABLE);
+        message = loggerMessageComponent.printMessage("error.register.response");
+        log.error("{} {}", message, HttpStatus.NOT_ACCEPTABLE);
         return mapResponseEntity;
     }
 }
