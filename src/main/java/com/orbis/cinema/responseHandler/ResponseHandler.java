2f4b68d4222fefaaa512d1f8ee560717676cc760
zaccalos7 <guarin719@outlook.it>
2025-11-19 21:35:56 +0100

    handled messages en and it

---
+++ src/main/java/com/orbis/cinema/responseHandler/ResponseHandler.java
@@ -0,0 +1,27 @@
+package com.orbis.cinema.responseHandler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Component
+public class ResponseHandler {
+    private final MessageSource messageSource;
+
+    public ResponseEntity<Map<String, String>> buildResponse(String code, HttpStatus status){
+        Map<String , String> response = new HashMap<>();
+
+        String localizedMessage = messageSource.getMessage(code, new Object[]{}, LocaleContextHolder.getLocale());
+        response.put("message", localizedMessage);
+        ResponseEntity<Map<String, String>> responseEntity = new ResponseEntity<>(response, status);
+        return responseEntity;
+    }
+
+}
