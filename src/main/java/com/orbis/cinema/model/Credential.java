6eaf49ecd7fc6ad9a6885fbe70bc5a898dcded56
zaccalos7 <guarin719@outlook.it>
2025-12-01 00:42:14 +0100

    added unique on email

--- src/main/java/com/orbis/cinema/model/Credential.java
+++ src/main/java/com/orbis/cinema/model/Credential.java
@@ -16,7 +16,7 @@ public class Credential {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer pkid;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
     @Column(nullable = false)
     private String password;
