a14aeb41338d69ad1e28eed47a0a33d8270533d8
zaccalos7 <guarin719@outlook.it>
2026-01-27 20:42:39 +0100

    added new query

--- src/main/java/com/orbis/cinema/repository/CredentialRepository.java
+++ src/main/java/com/orbis/cinema/repository/CredentialRepository.java
@@ -4,6 +4,9 @@ import com.orbis.cinema.model.Credential;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface CredentialRepository extends JpaRepository<Credential, Integer> {
+
+    Credential findByEmail(String email);
 }
