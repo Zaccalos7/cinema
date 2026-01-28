89abd1a823a76d74f0363d041339c46d7ea0d0c1
zaccalos7 <guarin719@outlook.it>
2026-01-28 21:18:12 +0100

    added new customexceptions

--- src/main/java/com/orbis/cinema/exceptions/HandlerExceptionInterface.java
+++ src/main/java/com/orbis/cinema/exceptions/HandlerExceptionInterface.java
@@ -3,7 +3,7 @@ package com.orbis.cinema.exceptions;
 import com.orbis.exception.annotations.ExceptionMaker;
 import com.orbis.exception.annotations.ExceptionRunner;
 
-@ExceptionMaker(classesName = {"NotValidException"})
+@ExceptionMaker(classesName = {"NotValidException", "NotValidCredentials"})
 public interface HandlerExceptionInterface {
 
     @ExceptionRunner(exceptionClass = "NotValidException", componentModel = "spring")
