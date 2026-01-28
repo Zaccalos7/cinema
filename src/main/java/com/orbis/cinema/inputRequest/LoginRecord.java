3d36eaa190703ddd04eb4ddc5bc1b95e1c80cd46
zaccalos7 <guarin719@outlook.it>
2026-01-28 21:16:28 +0100

    added password to record input

--- src/main/java/com/orbis/cinema/inputRequest/LoginRecord.java
+++ src/main/java/com/orbis/cinema/inputRequest/LoginRecord.java
@@ -2,5 +2,5 @@ package com.orbis.cinema.inputRequest;
 
 import jakarta.validation.constraints.NotNull;
 
-public record LoginRecord(@NotNull(message = "not.valid.input") String email) {
+public record LoginRecord(@NotNull(message = "not.valid.input") String email, @NotNull(message = "not.valid.input") String password) {
 }
