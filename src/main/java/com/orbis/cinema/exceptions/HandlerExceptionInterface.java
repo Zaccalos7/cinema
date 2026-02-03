8c02400084a913fa2fa4f0b953087d017e0a194c
zaccalos7 <guarin719@outlook.it>
2026-02-03 14:28:00 +0100

    removed filereading exceptions , to use restcontroller advice

--- src/main/java/com/orbis/cinema/exceptions/HandlerExceptionInterface.java
+++ src/main/java/com/orbis/cinema/exceptions/HandlerExceptionInterface.java
@@ -3,12 +3,10 @@ package com.orbis.cinema.exceptions;
 import com.orbis.exception.annotations.ExceptionMaker;
 import com.orbis.exception.annotations.ExceptionRunner;
 
-@ExceptionMaker(classesName = {"NotValidException", "FileReadingException"})
+@ExceptionMaker(classesName = {"NotValidException"})
 public interface HandlerExceptionInterface {
 
     @ExceptionRunner(exceptionClass = "NotValidException", componentModel = "spring")
     void runnerNotValidException(String message);
 
-    @ExceptionRunner(exceptionClass = "FileReadingException", componentModel = "spring")
-    String runnerFileReadingException(String message);
 }
