a78ac0ba77359e5c93edb17b1ee98b3482893600
zaccalos7 <guarin719@outlook.it>
2026-02-03 17:22:39 +0100

    inline variable

--- src/main/java/com/orbis/cinema/component/LoggerMessageComponent.java
+++ src/main/java/com/orbis/cinema/component/LoggerMessageComponent.java
@@ -11,12 +11,11 @@ public class LoggerMessageComponent {
     private final MessageSource messageSource;
 
     public String printMessage(String code){
-        String message = messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
-        return message;
+        return messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
     }
 
+
     public String printMessage(String code, Object[] params){
-        String message = messageSource.getMessage(code , params, LocaleContextHolder.getLocale());
-        return message;
+        return messageSource.getMessage(code , params, LocaleContextHolder.getLocale());
     }
 }
