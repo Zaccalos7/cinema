3c5e45c66dc0d1747fdae827f39ba6f73a9f96b3
zaccalos7 <guarin719@outlook.it>
2026-02-26 20:50:22 +0100

    removed unused package

--- src/main/java/com/orbis/cinema/handler/ExceptionsHandler.java
+++ src/main/java/com/orbis/cinema/handler/ExceptionsHandler.java
@@ -10,8 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
-
 
 import javax.security.auth.login.LoginException;
 import java.sql.SQLException;
